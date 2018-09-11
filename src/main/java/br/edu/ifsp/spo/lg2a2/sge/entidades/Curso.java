9c5fcec1213c310b86d822e8cb8da4bdaabca11b
feehlds <42723584+feehlds@users.noreply.github.com>
2018-09-10 22:39:51 -0300

    Add files via upload

--- src/main/java/br/edu/ifsp/spo/lg2a2/sge/entidades/Curso.java
+++ src/main/java/br/edu/ifsp/spo/lg2a2/sge/entidades/Curso.java
@@ -3,9 +3,12 @@ package br.edu.ifsp.spo.lg2a2.sge.entidades;
 import java.util.Collection;
 
 public class Curso {
+	private Collection<Turma> turmas;
+	private String codigoCurso;
 	
 	private Collection<Turma> turmas;
 	public Collection<Turma> getTurmas() {
+		this.turmas = turmas;
 		return this.turmas;
 	}
 
--- src/main/java/br/edu/ifsp/spo/lg2a2/sge/repositories/CursosRepository.java
+++ src/main/java/br/edu/ifsp/spo/lg2a2/sge/repositories/CursosRepository.java
@@ -1,14 +1,29 @@
 package br.edu.ifsp.spo.lg2a2.sge.repositories;
 
-import java.util.Collection;
-
+import java.util.*;
 import br.edu.ifsp.spo.lg2a2.sge.entidades.Aluno;
 import br.edu.ifsp.spo.lg2a2.sge.entidades.Curso;
 import br.edu.ifsp.spo.lg2a2.sge.entidades.Turma;
 
 public class CursosRepository {
+	private static Collection<Turma> turmas = new ArrayList<Turma>();
+	private static Collection<Curso> cursos = new ArrayList<Curso>();
+	private static Collection<Aluno> alunos = new ArrayList<Aluno>();
+
+	public CursosRepository(Collection<Turma> turmas)
+	{
+		this.turmas.addAll(turmas);
+	}
+
 	
 	public Curso buscarPorCodigo(String codigo) {
+		for(Curso curso : CursosRepository.cursos)
+		{
+			if(curso.getCodigoCurso().equals(codigo))
+			{
+				return curso;
+			}
+		}
 		return null;
 	}
 	
@@ -21,9 +36,23 @@ public class CursosRepository {
 	}
 	
 	public boolean existeAlunoNoCurso(String cpf) {
+		for(Aluno aluno : CursosRepository.alunos) {
+			if(aluno.getCpf().equals(cpf)) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	public void adicionarAluno(Aluno aluno) {
+		CursosRepository.alunos.add(aluno);
+	}
+
+	public static Collection<Curso> getCurso() {
+		return curso;
+	}
+
+	public static void setCurso(Collection<Curso> curso) {
+		CursosRepository.curso = curso;
 	}
 }
